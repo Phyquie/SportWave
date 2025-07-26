9bb600eb8ffccc0f92b4c9270e3e89bd2659c8cb
ayushbisht <ayushking6395@gmail.com>
2025-07-26 15:43:55 +0530

    added get direction button

--- app/myprofile/mybookedevents/page.js
+++ app/myprofile/mybookedevents/page.js
@@ -24,6 +24,7 @@ const page = () => {
                                         <h2 className='text-xl font-semibold'>{event.name}</h2>
                                         <p className='text-gray-600'>Date: {new Date(event.date).toLocaleDateString()}</p>
                                         <p className='text-gray-600'>Location: {event.location}</p>
+                                        <button className='bg-indigo-600 rounded-xl text-white py-2 px-4 mt-4'>Get Direction</button>
                                     </div>
                                     <div className=' ml-auto flex items-center px-4'>
                                         <img src={event.image_urls[0]} alt={event.name} className='w-36 h-20 rounded-md object-cover ml-4' />
